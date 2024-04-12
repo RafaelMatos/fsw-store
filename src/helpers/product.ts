74cd5e1969f5cf8020afee217f754acef845d75d
RafaelMatos <rafael-matos@outlook.com>
2024-04-12 06:08:46 -0300

    feat: add computeProductTotalPrice helper function

---
+++ src/helpers/product.ts
@@ -0,0 +1,23 @@
+import { Product } from '@prisma/client'
+interface ProductWithTotalPrice extends Product {
+  totalPrice: number
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const computeProductTotalPrice = (
+  product: Product,
+): ProductWithTotalPrice => {
+  if (product.discountPercentage === 0) {
+    return {
+      ...product,
+      totalPrice: Number(product.basePrice),
+    }
+  }
+  const totalPrice =
+    Number(product.basePrice) * (product.discountPercentage / 100)
+
+  return {
+    ...product,
+    totalPrice,
+  }
+}
