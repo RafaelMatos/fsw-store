30ed59b376991e0e5b68fc78fd09b5f14ccb70c5
RafaelMatos <rafael-matos@outlook.com>
2024-04-12 05:46:16 -0300

    chore: add image domains to next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'utfs.io',
+      },
+      {
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        hostname: 'github.com',
+      },
+    ],
+  },
+}
 
 export default nextConfig
