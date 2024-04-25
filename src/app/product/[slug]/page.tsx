3e9cc365abe6563a018f7c514dd68e44b4944760
RafaelMatos <rafael-matos@outlook.com>
2024-04-25 18:17:35 -0300

    feat: add recommended products list to product page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { prismaClient } from '@/lib/prisma'
 import ProductImages from './components/product-images'
 import ProductInfo from './components/product-info'
 import { computeProductTotalPrice } from '@/helpers/product'
+import ProductList from '@/components/ui/product-list'
+import SectionTitle from '@/components/ui/section-title'
 
 interface ProductDetailsPageProps {
   params: {
@@ -16,12 +18,35 @@ const ProductDetailsPage = async ({
     where: {
       slug,
     },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
   })
+
   if (!product) return null
   return (
-    <div className="flex flex-col gap-8 p-5 lg:grid lg:grid-cols-2">
-      <ProductImages imageUrls={product.imageUrls} productName={product.name} />
-      <ProductInfo product={computeProductTotalPrice(product)} />
+    <div className="flex flex-col gap-8  pb-8">
+      <div className="mt-5 flex flex-col gap-8 lg:mx-5  lg:grid lg:grid-cols-2">
+        <ProductImages
+          imageUrls={product.imageUrls}
+          productName={product.name}
+        />
+        <ProductInfo product={computeProductTotalPrice(product)} />
+      </div>
+      <div className=" flex flex-col">
+        <SectionTitle>Produtos Recomendados</SectionTitle>
+        <ProductList products={product.category.products} />
+      </div>
     </div>
   )
 }
